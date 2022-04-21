4ef22cda30958f26136c54db79d437e779877a5e
DMzartex <91669692+DMzartex@users.noreply.github.com>
2022-04-21 13:19:12 +0100

    Ajout de biblio

---
+++ gestion_biblio/views/login.php
@@ -0,0 +1,16 @@
+<div class="container"> 
+    <p>Connecter vous ici !</p>
+    <div class="block-form">
+        <div class="form">
+            <div>
+                <input type="text" placeholder="Pseudo :">
+            </div>
+            <div>
+                <input type="text" placeholder="Mot de passe :">
+            </div>
+            <div class="btn-form">
+                <input type="submit" value="Rechercher">
+            </div>
+        </div>
+    </div>
+</div>
